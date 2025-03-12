09921dfed3b8a1b76128ab17da588052d761e7ba
Alvin7-boys <alvinboys270702@gmail.com>
2025-03-13 00:11:21 +0700

    Extract the ErrorMessage Component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from 'zod';
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 // Lazy import SimpleMDE dengan SSR dinonaktifkan
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
@@ -37,7 +38,7 @@ const NewIssuePage = () => {
 
             })}>
                 <TextField.Root placeholder='Title' {...register('title')} />
-                {errors.title && <Text color="red" as="p" >{errors.title.message}</Text>}
+                <ErrorMessage >{errors.title?.message}</ErrorMessage>
                 <Controller
                     name="description"
                     control={control}
@@ -45,7 +46,7 @@ const NewIssuePage = () => {
                         <SimpleMDE placeholder='Description'  {...field} />
                     )}
                 />
-                {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
                 <Button type="submit">Submit New Issue</Button>
             </form>
         </div>
